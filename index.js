d4e95e6174fddb30284bc768af5b0e34d410f82e
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-05-14 14:59:42 -0500

    convert server with socket.io

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 
 let app = express();
 
-app.listen(4000, () => {
-  console.log("listening to port 4000");
-})
+app.use(express.static("public"));
 
+// Standard server - takes request and response communication
+let server = app.listen(4000, () => {
+  console.log("listening to port 4000");
+});
+
+// Wrapping server into socket.io to accpet bidirectional communication
+let newServer = socket(server);
+
+// Connecting the server to socket.io and get id when connected
+newServer.on('connection', (socket) => {
+  console.log('Websocket Connected', socket.id);
+});
