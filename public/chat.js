c7c9ff278989837b2a23399d7ae112bc42ae6b8a
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-05-14 15:22:27 -0500

    add code to send message to server

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,16 @@
+// Connect client to server
 let socket = io.connect("http://localhost:4000");
+
+// Getting DOM elements
+let message = document.getElementById("message");
+let button = document.getElementById('send');
+let username = document.getElementById('username');
+let output = document.getElementById('output');
+
+// Send message with message and username when the send button is clicked
+button.addEventListener('click', () => {
+  socket.emit('sendingMessage', {
+    message: message.value,
+    username: username.value
+  })
+})
